cef40858ff0550b1c536f9fa044c3886e64f6e18
Kartikeya Singh <kartikeya.2427030745@muj.manipal.edu>
2025-12-31 11:32:44 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import hashlib
 import secrets
 
 class Database:
-    def __init__(self, db_name="/data/commission_system.db"):  # Changed to /data/ for Render
+    def __init__(self, db_name="/tmp/commission_system.db"):  # Changed to /data/ for Render
         self.db_name = db_name
         self.init_db()
     
